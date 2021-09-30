ae1eec9329d1c64dd2d97eb9d5877d6572a1f240
cw58665466 <58665466@qq.com>
2021-09-30 16:34:36 +0800

    Update conf.go

--- conf/conf.go
+++ conf/conf.go
@@ -12,7 +12,7 @@ import (
 // Init 初始化配置项
 func Init() {
 	// 从本地读取环境变量
-	godotenv.Load("/etc/secret/.env")
+	godotenv.Load("/etc/secrets/.env")
 
 	// 设置日志级别
 	util.BuildLogger(os.Getenv("LOG_LEVEL"))
